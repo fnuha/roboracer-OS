a05aef6ea8e3ef8bbd52b76592351059a27f7971
Nuha <nuhaf@oregonstate.edu>
2025-06-04 17:36:20 -0700

    implemented freeRTOSin STM32

---
+++ Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/list.h
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/list.h:
---
+++ Debug/ThirdParty/FreeRTOS/stream_buffer.d
@@ -0,0 +1,22 @@
+ThirdParty/FreeRTOS/stream_buffer.o: \
+ ../ThirdParty/FreeRTOS/stream_buffer.c \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/stream_buffer.h
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/School/Desktop/2024-2025/Capstone/RTOS_Workspace/FreeRTOS_project/ThirdParty/FreeRTOS/include/stream_buffer.h:
